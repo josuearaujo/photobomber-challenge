263991a30968a5ad1b28bb3448d7d3b823a8aebe
Josué Araújo <josue22595@hotmail.com>
2023-10-16 16:51:46 -0400

    Create ImageSquare component

---
+++ resources/js/Components/ImageSquare.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-[360px] h-[360px] p-5 text-center">                                                       
+        <img :src=imgSrc class="h-[100%]" @load="onLoadImg"/>
+    </div>
+</template>
+
+<script setup>
+const onLoadImg = (e) => {console.log('Carregou!', e.currentTarget, e.returnValue, e.target, e)};
+defineProps({imgSrc: String});
+</script>
