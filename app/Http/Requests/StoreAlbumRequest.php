4a3281959c27d549ce9ddcdf2fe1f7721f3071f4
Josué Araújo <josue22595@hotmail.com>
2023-10-17 18:43:37 -0400

    Create Store Album Request

---
+++ app/Http/Requests/StoreAlbumRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreAlbumRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:50',
+            'description' => 'max:255',
+            'layout' => Rule::in([1,2, 4])
+        ];
+    }
+}
