9e3dd6a60519810350e27e81a834b44b9790f976
Josué Araújo <josue22595@hotmail.com>
2023-10-17 22:34:17 -0400

    Create component to truncate the text and show a tooltip

---
+++ resources/js/Components/TextWithTooltip.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div
+            class="text-sm truncate w-30"
+            @mouseenter="showTooltip = true"
+            @mouseleave="showTooltip = false"
+        >
+            {{ truncatedText }}
+        </div>
+        <div
+            v-if="showTooltip"
+            class="absolute bg-white p-2 shadow border border-gray-300 rounded-lg"
+        >
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const props = defineProps({text: String});
+const showTooltip = ref(false);
+
+const truncatedText = props.text.slice(0, 30) + (props.text.length > 30 ? '...' : '');
+</script>
