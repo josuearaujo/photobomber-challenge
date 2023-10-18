dc175f8288fe1d24a40acd12a091fd86189ca9e1
Josué Araújo <josue22595@hotmail.com>
2023-10-17 21:04:19 -0400

    Create initial show album page

---
+++ resources/js/Pages/Photobook/Show.vue
@@ -0,0 +1,32 @@
+<template>
+    <Head title="Photobook" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <div class="flex justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Photobook
+                </h2>
+            </div>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Show info!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import { Head } from '@inertiajs/vue3';
+import axios from "axios";
+
+const props = defineProps({albums: Array, token: String});
+axios.defaults.headers.common['Authorization'] = `Bearer ${props.token}`;
+</script>
