f467179b2a35859614bac49f237d37a77dbe2134
Josué Araújo <josue22595@hotmail.com>
2023-10-18 20:47:13 -0400

    Remove unused page

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-
-    return Inertia::render('Dashboard', );
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/photos', [PhotoController::class, 'index'])->name('photo.index');
     Route::get('/photos/{photo}', [PhotoController::class, 'show'])->name('photo.show');
